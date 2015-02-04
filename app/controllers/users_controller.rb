20aebaea50949c9a4fc39e545af96fba8a141ad3
maraya29 <maraya10>
2015-02-04 10:17:51 -0600

    allowing update user fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :new, :edit)
+      params.require(:user).permit(:email, :password, :first_name, :last_name, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
   acts_as_authentic do |config|
     config.login_field = :email
     config.merge_validates_length_of_password_field_options({:minimum => 6, :maximum => 20})
-    config.validate_email_field = false
   end 
-  
+
   validates_presence_of :first_name, :last_name, :email
   
   def full_name
